fa174d76b58e89cf8fde051216e6f322eccb23b3
guntar <guntar@me.com>
2024-01-28 16:49:52 +0700

    edit allowed server

--- core/settings.py
+++ core/settings.py
@@ -29,8 +29,9 @@ DEBUG = 'RENDER' not in os.environ
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets')
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', 'localhost:4000', '127.0.0.1', '157.245.156.179', '157.245.156.179:4080',  env('SERVER', default='127.0.0.1')]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:4000', 'http://127.0.0.1', '157.245.156.179:4080', 'https://' + env('SERVER', default='127.0.0.1') ]
+ALLOWED_HOSTS = ['localhost', 'localhost:8000', '127.0.0.1', '134.209.108.83',
+                 '134.209.108.83:8000',  env('SERVER', default='127.0.0.1')]
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://127.0.0.1', '134.209.108.83:8000', 'https://' + env('SERVER', default='127.0.0.1') ]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
